58f02015f227f89952d1d4a06d1950053fb98209
lazzzy-coder <aman.pandey@imsuc.ac.in>
2022-03-28 02:53:29 +0530

    first commit

---
+++ blog/views.py
@@ -0,0 +1,90 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views.generic import (
+    CreateView,
+    DetailView,
+    ListView,
+    UpdateView,
+    ListView,
+    DeleteView
+)
+
+from .forms import ArticleModelForm
+from .models import Article
+
+
+class ArticleCreateView(CreateView):
+    template_name = 'articles/article_create.html'
+    form_class = ArticleModelForm
+    queryset = Article.objects.all() # <blog>/<modelname>_list.html
+    #success_url = '/'
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
+
+    #def get_success_url(self):
+    #    return '/'
+
+class ArticleListView(ListView):
+    template_name = 'articles/article_list.html'
+    queryset = Article.objects.all() # <blog>/<modelname>_list.html
+
+
+class ArticleDetailView(DetailView):
+    template_name = 'articles/article_detail.html'
+    #queryset = Article.objects.all()
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
+
+
+class ArticleUpdateView(UpdateView):
+    template_name = 'articles/article_create.html'
+    form_class = ArticleModelForm
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
+
+
+class ArticleDeleteView(DeleteView):
+    template_name = 'articles/article_delete.html'
+    
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
+
+    def get_success_url(self):
+        return reverse('articles:article-list')
+
+def home_view(request, *args, **kwargs): # *args, **kwargs
+    print(args, kwargs)
+    print(request.user)
+    #return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+    return render(request, "home.html", {})
+
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
+
+
+def about_view(request, *args, **kwargs):
+    my_context = {
+        "title": "abc this is about us",
+        "this_is_true": True,
+        "my_number": 123,
+        "my_list": [1313, 4231, 312, "Abc"],
+        "my_html": "<h1>Hello World</h1>"
+
+    }
+    return render(request, "about.html", my_context)
+
+
+def social_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Socail Page</h1>")
---
+++ trydjango/urls.py
@@ -0,0 +1,41 @@
+"""trydjango URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+
+from blog.views import(
+    ArticleListView,
+    ArticleDetailView,
+    ArticleCreateView,
+    ArticleUpdateView,
+    ArticleDeleteView,
+    home_view,
+    contact_view,
+    about_view
+)
+
+urlpatterns = [
+    path('blog/', include('blog.urls')),
+    path('', ArticleListView.as_view(), name='article-list'),
+    path('create/', ArticleCreateView.as_view(), name='article-create'),
+    path('<int:id>/', ArticleDetailView.as_view(), name='article-detail'),
+    path('<int:id>/update/', ArticleUpdateView.as_view(), name='article-update'),
+    path('<int:id>/delete/', ArticleDeleteView.as_view(), name='article-delete'),
+    path('about/', about_view, name='about-view'),
+    path('contact/', contact_view,name='contact-view'),
+    path('admin/', admin.site.urls),
+    
+]
